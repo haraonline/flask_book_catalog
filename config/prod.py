9ca5fde2aa4d52e0e47e818bb52444cd1d89c3aa
haraonline <haragopal.kovvali@gmail.com>
2017-08-31 21:09:35 +0530

    removed db password

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,4 @@
 DEBUG = False
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:topsecret@localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://catalog_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
