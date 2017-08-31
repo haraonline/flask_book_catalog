95b89a32f1c3ad030b92e2baed1f51375ff29ae5
haraonline <haragopal.kovvali@gmail.com>
2017-08-31 17:21:40 +0530

    edited database url

--- config/dev.py
+++ config/dev.py
@@ -1,4 +1,7 @@
+import os
+
 DEBUG = True
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:topsecret@localhost/catalog_db'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:topsecret@localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI = os.environ('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
