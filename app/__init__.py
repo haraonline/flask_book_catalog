49e14775bd6b434cccedd6f92c94414e9bf7c1d8
haraonline <haragopal.kovvali@gmail.com>
2017-08-31 21:48:46 +0530

    installed flask-heroku

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from flask_heroku import Heroku
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
@@ -12,6 +13,7 @@ login_manager = LoginManager()
 login_manager.login_view = 'authentication.do_the_login'
 login_manager.session_protection = 'strong'
 bcrypt = Bcrypt()
+heroku = Heroku()
 
 
 def create_app(config_type):  # dev, test, or prod
@@ -24,6 +26,7 @@ def create_app(config_type):  # dev, test, or prod
     bootstrap.init_app(app)  # initialize bootstrap
     login_manager.init_app(app)  # initialize login_manager
     bcrypt.init_app(app)
+    heroku.init_app(app)
 
     from app.catalog import main  # import blueprint
     app.register_blueprint(main)  # register blueprint
--- config/prod.py
+++ config/prod.py
@@ -1,6 +1,3 @@
-import os
-
 DEBUG = False
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
