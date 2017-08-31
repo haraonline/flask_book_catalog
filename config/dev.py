70e3828233ecbf8f1c10b41bc4c98e6bc92f9544
haraonline <haragopal.kovvali@gmail.com>
2017-08-31 17:42:58 +0530

    added psycopg2

--- config/dev.py
+++ config/dev.py
@@ -1,7 +1,4 @@
-import os
-
 DEBUG = True
 SECRET_KEY = 'topsecret'
-# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:topsecret@localhost/catalog_db'
-SQLALCHEMY_DATABASE_URI = os.environ('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:topsecret@localhost/catalog_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
