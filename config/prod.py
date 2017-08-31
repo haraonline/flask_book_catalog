c6b964a925bca15e55ebc97c716e743647ba9596
haraonline <haragopal.kovvali@gmail.com>
2017-08-31 21:37:37 +0530

    added os.environ

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,6 @@
+import os
+
 DEBUG = False
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:topsecret/catalog_db'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
