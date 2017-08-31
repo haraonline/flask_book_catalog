650d06de3195aa7ca85d1c21eb70e99e7fea7413
haraonline <haragopal.kovvali@gmail.com>
2017-08-31 16:40:53 +0530

    changed if name is main

--- run.py
+++ run.py
@@ -1,10 +1,18 @@
 from app import create_app, db
 from app.auth.models import User
 
-if __name__ == '__main__':
-    flask_app = create_app('prod')
-    with flask_app.app_context():
-        db.create_all()
-        if not User.query.filter_by(user_name='harry').first():
-            User.create_user(user='harry', email='harry@potters.com', password='secret')
+# if __name__ == '__main__':
+#     flask_app = create_app('prod')
+#     with flask_app.app_context():
+#         db.create_all()
+#         if not User.query.filter_by(user_name='harry').first():
+#             User.create_user(user='harry', email='harry@potters.com', password='secret')
+#     flask_app.run()
+
+
+flask_app = create_app('prod')
+with flask_app.app_context():
+    db.create_all()
+    if not User.query.filter_by(user_name='harry').first():
+        User.create_user(user='harry', email='harry@potters.com', password='secret')
     flask_app.run()
