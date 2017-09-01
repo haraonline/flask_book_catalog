b6279672a879b1733ca234e7acf7d5c236b5446a
haraonline <haragopal.kovvali@gmail.com>
2017-09-01 09:16:30 +0530

    added database uri

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,4 @@
 DEBUG = False
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI = 'postgres://emluhothsjtgbd:396aa79d1a536127579578b17c7b0d31dee39adb95c5af8ba465c9aeae2951e2@ec2-23-23-86-179.compute-1.amazonaws.com:5432/de0fsck3g4i5b7'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
