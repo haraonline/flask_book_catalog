828f70496659d2c2c5dcb81f1d12db968f172116
haraonline <haragopal.kovvali@gmail.com>
2017-09-01 21:49:21 +0530

    some changes

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from app import create_app, db
 from app.auth.models import User
 
-if __name__ == '__main__':
-    flask_app = create_app('prod')
-    with flask_app.app_context():
-        db.create_all()
-        if not User.query.filter_by(user_name='harry').first():
-            User.create_user(user='harry', email='harry@potters.com', password='secret')
-    flask_app.run(host='0.0.0.0', port=5000)
+
+flask_app = create_app('prod')
+with flask_app.app_context():
+    db.create_all()
+    if not User.query.filter_by(user_name='harry').first():
+        User.create_user(user='harry', email='harry@potters.com', password='secret')
+flask_app.run()
