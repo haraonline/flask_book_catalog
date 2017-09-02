0cc948d9d2e0ffe3646ac8c84c0d1224812fb8c4
haraonline <haragopal.kovvali@gmail.com>
2017-09-02 16:29:59 +0530

    deploy to heroku

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
 from app import create_app, db
 from app.auth.models import User
 
-if __name__ == '__main__':
-    flask_app = create_app('prod')
-    with flask_app.app_context():
-        db.create_all()
-        # if not User.query.filter_by(user_name='harry').first():
-        #     User.create_user(user='harry', email='harry@potters.com', password='secret')
+flask_app = create_app('prod')
+with flask_app.app_context():
+    db.create_all()
     flask_app.run()
