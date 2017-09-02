31dd0bf66da115bc5dc0bdc75459954dc8d93f01
haraonline <haragopal.kovvali@gmail.com>
2017-09-02 16:42:29 +0530

    more changes

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,4 @@
 DEBUG = False
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = 'postgres://localhost/user_db'
+SQLALCHEMY_DATABASE_URI = 'postgres://rewytsohtsjprl:77c5a1238ce1f66244aadec91eb84708f6b68e87ec854eaaba77a20406b6b0b3@ec2-54-235-177-45.compute-1.amazonaws.com:5432/dco155p080r0gi'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- run.py
+++ run.py
@@ -4,4 +4,6 @@ from app.auth.models import User
 flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
+    if not User.query.filter_by(user_name='harry').first():
+        User.create_user(user='harry', email='harray@potters.com', password='secret')
     flask_app.run()
