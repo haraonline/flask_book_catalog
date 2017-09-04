a0c8236fb9fd9f9dc7a3a1e8479096de265c381d
haraonline <haragopal.kovvali@gmail.com>
2017-09-04 20:35:44 +0530

    urlparse

--- config/prod.py
+++ config/prod.py
@@ -1,6 +1,6 @@
-import os
+from urllib.parse import urlparse
 
 DEBUG = False
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = os.environ(['DATABASE_URL'])
+SQLALCHEMY_DATABASE_URI = urlparse('DATABASE_URL').netloc
 SQLALCHEMY_TRACK_MODIFICATIONS = False
