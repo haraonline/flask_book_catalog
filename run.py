0e2c7ff1f0d271992a66266a8400661d7bbf87e9
haraonline <haragopal.kovvali@gmail.com>
2017-09-04 17:32:54 +0530

    testing db

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
     if not User.query.filter_by(user_name='harry').first():
-        User.create_user(user='harry', email='harray@potters.com', password='secret')
+        User.create_user(user='harry', email='harry@potters.com', password='secret')
         flask_app.run()
