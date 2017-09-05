4c5a14bf79496953d2485b8c6dc32c198dde2b1c
haraonline <haragopal.kovvali@gmail.com>
2017-09-05 20:34:17 +0530

    changed user name

--- run.py
+++ run.py
@@ -6,6 +6,6 @@ with flask_app.app_context():
     db.create_all()
     if not User.query.filter_by(user_name='test user').first():
         User.create_user(user='test', email='test@test.com', password='secret')
-    flask_app.run()
+        flask_app.run()
 
 
