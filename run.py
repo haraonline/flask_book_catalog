348c8abacf5c24afcee0a3f93bb8ec0088ba2c8a
haraonline <haragopal.kovvali@gmail.com>
2017-09-05 18:48:03 +0530

    changed

--- run.py
+++ run.py
@@ -2,8 +2,7 @@ from app import create_app, db
 from app.auth.models import User
 
 flask_app = create_app('prod')
-with flask_app.app_context():
-    db.create_all()
-    flask_app.run()
+db.create_all()
+flask_app.run()
 
 
