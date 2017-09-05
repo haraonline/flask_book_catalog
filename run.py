0f80290977664db11c1f6534d1cb12781af09bd5
haraonline <haragopal.kovvali@gmail.com>
2017-09-05 21:47:10 +0530

    make it better

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from app.auth.models import User
 flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
-    if User.query.filter_by(user_name='harry').first() is None:
+    if not User.query.filter_by(user_name='harry').first():
         User.create_user(user='harry', email='harry@potters.com', password='secret')
         flask_app.run()
 
