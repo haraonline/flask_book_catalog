a0bace005180117fe91141f1424c94fa25f10df6
haraonline <haragopal.kovvali@gmail.com>
2017-09-05 20:06:30 +0530

    clean

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from app.auth.models import User
 
 flask_app = create_app('prod')
 with flask_app.app_context():
-    db.create_all()
-flask_app.run()
+    db.create_all(flask_app)
+    flask_app.run()
 
 
